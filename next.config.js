58b01d65341a5b419d9f2e98a14f791956355f51
Rob Berry <robert.berry@gmail.com>
2022-01-14 23:46:51 +0000

    netlify performace

--- next.config.js
+++ next.config.js
@@ -2,41 +2,8 @@ module.exports = {
   poweredByHeader: false,
   trailingSlash: true,
   reactStrictMode: true,
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-  images: {
-    minimumCacheTTL: 3600,
-  },
-  async headers() {
-    return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-Frame-Options',
-              value: 'DENY',
-            },
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'Referrer-Policy',
-              value: 'origin-when-cross-origin',
-            },
-            {
-              key: 'Permissions-Policy',
-              value: "camera=(); battery=(self); geolocation=(); microphone=()",
-            },
-          ],
-        },
-      ];
-    },
-  };
-
+}
