d0dad15e36202d561e9c8311573362d882b4280d
Rob Berry <robert.berry@gmail.com>
2022-03-24 11:06:21 +0000

    Try setting headers in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,52 @@
+const ContentSecurityPolicy = `
+  default-src 'self'; 
+  script-src 'report-sample' 'self';
+  style-src 'report-sample' 'self';
+  object-src 'self'; base-uri 'self'; 
+  connect-src 'self';
+  font-src 'self'; 
+  frame-src 'self'; 
+  img-src 'self' data:;
+  manifest-src 'self'; 
+  media-src 'self';
+  report-uri https://61e3325cb87c360d6d96d810.endpoint.csper.io/?v=1; 
+  worker-src 'none';
+`
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
+
 module.exports = {
   poweredByHeader: false,
   trailingSlash: true,
@@ -13,4 +62,13 @@ module.exports = {
 
     return config
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  }
 }
