1be2cab5d0bd0068c9428343fadb89acb561e36e
Rob Berry <robert.berry@gmail.com>
2022-04-06 20:50:04 +0100

    Remove i18n

--- next.config.js
+++ next.config.js
@@ -51,10 +51,6 @@ module.exports = {
   poweredByHeader: false,
   trailingSlash: true,
   reactStrictMode: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       require('./scripts/generate-sitemap')
