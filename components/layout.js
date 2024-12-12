af6cab8255c6fb507e491db9762f08a1c72461b0
Rob Berry <rob.berry@jbaconsulting.com>
2024-12-12 13:11:07 +0000

    Fix meta tags

--- components/layout.js
+++ components/layout.js
@@ -10,8 +10,8 @@ export default function Layout({ children }) {
     return (
         <>
             <Head>
-                <meta charset="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta charSet="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="copyright" content={`Copyright © ${new Date().getFullYear()} Rob Berry`} />
 
