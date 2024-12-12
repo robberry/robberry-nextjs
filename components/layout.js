e7a62cb3ccce24a9d207ee65cf4d243f2ddb84e0
Rob Berry <rob.berry@jbaconsulting.com>
2024-12-12 13:04:59 +0000

    Update meta tags

--- components/layout.js
+++ components/layout.js
@@ -10,19 +10,19 @@ export default function Layout({ children }) {
     return (
         <>
             <Head>
-                <meta charSet="utf-8" />
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="copyright" content={`Copyright © ${new Date().getFullYear()} Rob Berry`} />
 
-                <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-
+                <meta name="mobile-web-app-capable" content="yes" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
 
+                <meta name="theme-color" content="#343838" />
+                <meta name="author" content="Rob Berry" />
                 <meta property="og:type" content="website" />
                 <meta property="og:image" content="https://robberry.net/assets/images/icon.png" />
                 <meta property="twitter:card" content="summary" />
